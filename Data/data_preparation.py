272dd4e9e93cf90f3c0af31fe5b73eeb70a5f321
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2021-12-04 18:57:43 +0100

    standardise function

--- Data/data_preparation.py
+++ Data/data_preparation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+from sklearn.preprocessing import StandardScaler
 
 
 def load_dataset(filepath):
@@ -24,3 +24,27 @@ def categorical_to_dummy(dataframe, variables2convert, verbose=False):
 def paired_plot(dataframe, target):
     sns.pairplot(dataframe, hue=target)
     plt.show()
+
+
+def standardise(dataframe, features, verbose=False):
+    """
+    Applies the sklearn.preprocessing.StandardScaler to the features selected
+    :param dataframe: the dataframe containing the variables to scale
+    :param features: a list of all the attributes to be scaled
+    :param verbose: True to print some information on the execution
+    :return: the dataset with the converted attributes and the StandardScaler() fitted
+    """
+    scaler = StandardScaler()
+    dataframe_stand = dataframe.copy()  # copy to keep the variables that should not be scaled
+    scaler.fit(dataframe_stand.loc[:, :].astype(float))
+    dataframe_stand = pd.DataFrame(scaler.transform(dataframe_stand.loc[:, :].astype(float)))
+    dataframe_stand.columns = dataframe.columns
+
+    dataframe[features] = dataframe_stand[features]
+
+    if verbose is True:
+        dataframe_stand.hist()
+        plt.show()
+        print(dataframe.head(5))
+
+    return dataframe, scaler
