5a0513ad234eaae82abd2dc12f3029c8c94e0a45
Davide-Console <davideconsole99@gmail.com>
2021-12-04 11:23:49 +0100

    correlation and data_preparation

---
+++ Data/data_preparation.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+def load_dataset(filepath):
+    df = pd.read_csv(filepath, index_col=0)
+    return df
+
+
+def categorical_to_dummy(dataframe, variables2convert, verbose=False):
+    for variable in variables2convert:
+        dummy = pd.get_dummies(dataframe[variable], drop_first=True)
+        dataframe = dataframe.drop([variable], axis=1, inplace=False)
+        dataframe = pd.concat([dataframe, dummy], axis=1)
+
+    if verbose is True:
+        print(dataframe.head(5))
+
+    return dataframe
+
+
+def paired_plot(dataframe, target):
+    sns.pairplot(dataframe, hue=target)
+    plt.show()
